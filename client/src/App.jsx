58fbf77e27f320b41d0f09c0df1dc64be63d182c
Yash <yashgithug907@gmail.com>
2025-05-02 19:58:39 +0530

    adding back the backend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('https://chata-app-cb2w.onrender.com');
+const socket = io('http://localhost:5000');
 
 function App() {
   const [username, setUsername] = useState('');
