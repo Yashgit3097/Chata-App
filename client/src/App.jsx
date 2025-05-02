570aafb3df7b40f84d3d036d293eae4e2ed065bf
Yash <yashgithug907@gmail.com>
2025-05-02 19:51:42 +0530

    adding live backend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://chata-app-cb2w.onrender.com');
 
 function App() {
   const [username, setUsername] = useState('');
